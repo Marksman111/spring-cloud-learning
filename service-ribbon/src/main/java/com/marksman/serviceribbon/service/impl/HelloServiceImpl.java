ecf2ba4a1d13175821f542fbe66de2c5ab88703f
Marksman111 <wlb825077462@gmail.com>
2018-08-31 23:20:31 +0800

    hystrix

--- service-ribbon/src/main/java/com/marksman/serviceribbon/service/impl/HelloServiceImpl.java
+++ service-ribbon/src/main/java/com/marksman/serviceribbon/service/impl/HelloServiceImpl.java
@@ -1,6 +1,7 @@
 package com.marksman.serviceribbon.service.impl;
 
 import com.marksman.serviceribbon.service.HelloService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,8 +17,13 @@ public class HelloServiceImpl implements HelloService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     @Override
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name){
+        return "Hi," + name + ",sorry,error!";
     }
 }
