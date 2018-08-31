2aa4fcede9af3331b55945ee60f42bf523f77619
Marksman111 <wlb825077462@gmail.com>
2018-08-31 22:26:04 +0800

    ribbon

--- service-ribbon/src/main/java/com/marksman/serviceribbon/web/HelloController.java
+++ service-ribbon/src/main/java/com/marksman/serviceribbon/web/HelloController.java
@@ -2,6 +2,8 @@ package com.marksman.serviceribbon.web;
 
 import com.marksman.serviceribbon.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,4 +16,9 @@ public class HelloController {
 
     @Autowired
     private HelloService helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
 }
