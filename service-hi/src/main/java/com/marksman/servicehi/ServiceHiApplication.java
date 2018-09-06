33208fc96f3c75542258f72becd6f2f0d48354b4
Marksman111 <wlb825077462@gmail.com>
2018-09-06 21:55:01 +0800

    hystrix

--- service-hi/src/main/java/com/marksman/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/marksman/servicehi/ServiceHiApplication.java
@@ -1,18 +1,23 @@
 package com.marksman.servicehi;
 
-import brave.sampler.Sampler;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-
+@EnableCircuitBreaker
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@EnableHystrix
 @RestController
 @SpringBootApplication
 public class ServiceHiApplication {
@@ -21,34 +26,16 @@ public class ServiceHiApplication {
         SpringApplication.run(ServiceHiApplication.class, args);
     }
 
-    private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @Bean
-    public RestTemplate getRestTemplate(){
-        return new RestTemplate();
-    }
+    @Value("${server.port}")
+    String port;
 
     @RequestMapping("/hi")
-    public String callHome(){
-        LOG.log(Level.INFO,"calling trace service-hi");
-        return restTemplate.getForObject("http://localhost:8989/miya",String.class);
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String home(@RequestParam(value = "name",defaultValue = "marksman") String name){
+        return "hi " + name +" ,i am from port:" + port;
     }
 
-    @RequestMapping("/info")
-    public String info(){
-        LOG.log(Level.INFO,"calling trace service-hi");
-        return "I'm service-hi";
+    public String hiError(String name){
+        return "hi," + name + ",sorry,error";
     }
-
-    @Bean
-    public Sampler defaultSampler(){
-        return Sampler.ALWAYS_SAMPLE;
-    }
-
-
-
-
 }
